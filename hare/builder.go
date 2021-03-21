a8f060e224d1f9752852ad48fba4f57633f1e22e
Alexey Sudachen <alexey@sudachen.name>
2021-03-21 16:44:24 -0300

    message validation test fixes

--- hare/builder.go
+++ hare/builder.go
@@ -91,7 +91,6 @@ type messageBuilder struct {
 func newMessageBuilder() *messageBuilder {
 	m := &messageBuilder{&Msg{&Message{}, nil}, &innerMessage{}}
 	m.msg.InnerMsg = m.inner
-	m.inner.EligibilityCount = 1
 	return m
 }
 
